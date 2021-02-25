81535b303122d39eb9c94a917e1376a58615d8b9
PhillipHoejbjerg <58687573+PhillipHoejbjerg@users.noreply.github.com>
2021-02-25 16:26:24 +0100

    update

--- Gaussian_miniproject/api.py
+++ Gaussian_miniproject/api.py
@@ -4,12 +4,10 @@ from pprint import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import ParameterSampler, RandomizedSearchCV, cross_val_score
-import time, random, GPy, GPyOpt
+import time, random, GPyOpt
 
 # API KEY
 API_key = "62b8e1f5175f87ba1db6d6968a7e3ac1"
-
-# HEJ PHILLIP, DIN DOVNE NØD!
 
 # Code obtained from https://openweathermap.org/api/one-call-api
 current_time = int(time.time()) - 24*3600 # yesterday
@@ -29,9 +27,11 @@ def WindSpeed(lat, lon, current_time):
 
 
 ## define the domain of the considered parameters
-lon = (-25, -13)
-# print(n_estimators)
+# lat = (37, 41)
+# lon = (-109, -102)
+
 lat = (63, 67)
+lon = (-25, -13)
 
 # define the dictionary for GPyOpt
 domain = [{'name': 'lon', 'type': 'continuous', 'domain': lon},
@@ -48,7 +48,7 @@ def objective_function(x):
     # create the model
     model = WindSpeed(lat, lon, current_time)
 
-    return model
+    return - model # BO want to minimize this. Thus -
 
 
 opt = GPyOpt.methods.BayesianOptimization(f=objective_function,  # function to optimize
@@ -58,11 +58,21 @@ opt = GPyOpt.methods.BayesianOptimization(f=objective_function,  # function to o
 
 opt.acquisition.exploration_weight=0.5
 
-opt.run_optimization(max_iter = 15)
+opt.run_optimization(max_iter = 100)
 
 x_best = opt.X[np.argmin(opt.Y)]
 
 print("Coordinates with largest wind speed: latitude=" + str(x_best[1]) + ", longitude=" + str(x_best[0]))
+
+# Plot of guesses
+lon_plot, lat_plot = zip(*opt.X)
+plt.plot(lon_plot, lat_plot, "--o")
+xs = np.arange(len(lon_plot))
+for i, (x, y) in enumerate(zip(lon_plot, lat_plot)):
+    plt.text(x, y, str(xs[i]+1), color="black", fontsize=12)
+plt.margins(0.1)
+
+
 
 """
 # This stores the url
