44df81161a4d9dc4d4f11430a0a07748d5119603
PhillipHoejbjerg <58687573+PhillipHoejbjerg@users.noreply.github.com>
2021-03-01 21:36:33 +0100

    Update?

--- Gaussian_miniproject/api.py
+++ Gaussian_miniproject/api.py
@@ -8,7 +8,7 @@ import time, random, GPyOpt
 
 # API KEY
 # Remember to change key!
-API_key = "62b8e1f5175f87ba1db6d6968a7e3ac1"
+API_key = "9ac6c4296dd622e62a09753f5abe17b3"
 
 # Code obtained from https://openweathermap.org/api/one-call-api
 current_time = int(time.time()) - 24*3600 # yesterday
@@ -22,7 +22,7 @@ def WindSpeed(lat, lon, current_time):
 
     weather_data = requests.get(Final_url).json()
 
-    windspeed = weather_data['hourly'][12]['wind_speed'] #wind-speed, yesterday at 12
+    windspeed = weather_data['hourly'][12]['temp'] #wind-speed, yesterday at 12
     return windspeed
 
 
@@ -32,8 +32,12 @@ def WindSpeed(lat, lon, current_time):
 # lon = (-109, -102)
 
 #Specifying bounds of europe.
-lat = (36, 71)
-lon = (-9, 37)
+#lat = (36, 71)
+#lon = (-9, 37)
+
+#Bounds of iceland
+lat = (63, 67)
+lon = (-25, -13)
 
 # define the dictionary for GPyOpt
 domain = [{'name': 'lon', 'type': 'continuous', 'domain': lon},
@@ -53,40 +57,110 @@ def objective_function(x):
     return - model # BO want to minimize this. Thus -
 
 # Do random assignment of initial latitude and longitude ...
-np.random.seed(20)
-start_size = 50
+np.random.seed(50)
+start_size = 10
 lat_rand, lon_rand = np.random.uniform(low=lat[0],high=lat[1], size=start_size), np.random.uniform(low=lon[0],high=lon[1], size=start_size)
 
 X_init = np.array([list(x) for x in zip(lat_rand,lon_rand)])
 y_init = np.array([objective_function([x]) for x in X_init]).reshape(-1,1)
 
-
+# BO search:
 acquisition_functions = ['MPI', 'EI', 'LCB']
 exploration_values = [0.01, 0.1, 0.5, 1]
+#exploration_values = [2, 5, 10, 20]
 
-for a_function in acquisition_functions:
-    for i, exp_val in enumerate(exploration_values):
-        opt = GPyOpt.methods.BayesianOptimization(f=objective_function,  # function to optimize
-                                                  domain=domain,
-                                                  X=X_init, Y=y_init,# box-constrains of the problem
-                                                  acquisition_type=a_function,  # Select acquisition function MPI, EI, LCB
-                                                  )
+def BO_parameter_opt(N, X_init = X_init, y_init = y_init, acquisition_functions = acquisition_functions, exploration_values = exploration_values):
 
-        opt.acquisition.exploration_weight=exp_val
-        # See documentation of GPyOpt.models.base.BOModel() to see kernel type
+    cur_best_y = 0
+    cur_best_ys = np.zeros(len(y_init) + N)
 
-        opt.run_optimization(max_iter = 100)
+    for a_function in acquisition_functions:
+        for i, exp_val in enumerate(exploration_values):
+            opt = GPyOpt.methods.BayesianOptimization(f=objective_function,  # function to optimize
+                                                      domain=domain,
+                                                      X=X_init, Y=y_init,# box-constrains of the problem
+                                                      acquisition_type=a_function,  # Select acquisition function MPI, EI, LCB
+                                                      )
 
-        x_best = opt.X[np.argmin(opt.Y)]
+            opt.acquisition.exploration_weight=exp_val
+            # See documentation of GPyOpt.models.base.BOModel() to see kernel type
 
-        opt.plot_acquisition("modelPlot_"+a_function+"_expvalNo="+str(i), label_x="Longitude", label_y="Latitude")
-        opt.plot_convergence("convergencePlot_"+a_function+"_expvalNo="+str(i))
+            opt.run_optimization(max_iter = N)
 
-        print("Coordinates with largest wind speed: latitude=" + str(x_best[1]) + ", longitude=" + str(x_best[0])
-              + "\nAcquistion function = "+a_function + ", exploration weight = " + str(exp_val))
-        print("="*90)
+            print("min =", np.min(opt.Y), "expval =", exp_val, "a_func = ", a_function)
+
+            if np.min(opt.Y) < cur_best_y:
+                cur_best_y = np.min(opt.Y)
+                cur_best_ys = opt.Y
+                cur_best_acq = a_function
+                cur_best_exp = exp_val
+                cur_best_x = opt.X[np.argmin(opt.Y)]
+
+            '''
+            x_best = opt.X[np.argmin(opt.Y)]
+            opt.plot_acquisition("modelPlot_"+a_function+"_expvalNo="+str(i), label_x="Longitude", label_y="Latitude")
+            opt.plot_convergence("convergencePlot_"+a_function+"_expvalNo="+str(i))
+    
+            print("Coordinates with largest wind speed: latitude=" + str(x_best[1]) + ", longitude=" + str(x_best[0])
+                  + "\nAcquistion function = "+a_function + ", exploration weight = " + str(exp_val))
+            print("="*90)
+            '''
+    return cur_best_y, cur_best_ys, cur_best_x, cur_best_acq, cur_best_exp
 
 
+
+# Random search:
+
+def random_func(start_size, X_init = X_init, y_init = y_init):
+    lat_rand, lon_rand = np.random.uniform(low=lat[0],high=lat[1], size=start_size), np.random.uniform(low=lon[0],high=lon[1], size=start_size)
+
+    X_rand = np.array([list(x) for x in zip(lat_rand,lon_rand)])
+    y_rand = [float(objective_function([x])) for x in X_rand]
+
+    X_rand, y_rand = np.concatenate((X_init, X_rand)), np.concatenate(([y_init[i][0] for i in range(len(y_init))], y_rand))
+
+    X_rand_best, y_rand_best = X_rand[np.argmin(y_rand)], np.min(y_rand)
+
+    return X_rand, y_rand, X_rand_best, y_rand_best
+
+
+# Plot comparison:
+def plot_comparison(N, X_init = X_init, y_init = y_init):
+
+    _, BO_best_ys, x_best, cur_best_acq, cur_best_exp = BO_parameter_opt(N)
+
+    # Random:
+    rand_best_ys = random_func(N)[1]
+
+
+    rand_cur_best, BO_cur_best = np.zeros(N), np.zeros(N)
+    for i in range(N):
+        rand_cur_best[i] = np.min(rand_best_ys[:len(y_init) + i]) # Tager alle de initial, og finder den current bedste over vores iterations.
+        BO_cur_best[i] = np.min(BO_best_ys[:len(y_init) + i])
+
+    iterations = np.arange(0, N, 1)
+    plt.plot(iterations, rand_cur_best, 'o-', color = 'red', label = 'Random Search')
+    plt.plot(iterations, BO_cur_best, 'o-', color='blue', label='Bayesian Optimization')
+    plt.legend()
+    plt.xlabel('Iterations')
+    plt.ylabel('Windspeed')
+    plt.title('Comparison between Random Search and Bayesian Optimization')
+    plt.show()
+
+    return BO_best_ys, rand_best_ys, BO_best_ys, x_best, cur_best_acq, cur_best_exp
+
+
+BO_best_ys, rand_best_ys, BO_best_ys, x_best, cur_best_acq, cur_best_exp = plot_comparison(20)
+
+#Plotting acquisition function, convergence:
+opt.plot_acquisition("modelPlot_" + cur_best_acq + "_expval=" + str(cur_best_exp), label_x="Longitude", label_y="Latitude")
+plt.show
+opt.plot_convergence("convergencePlot_" + cur_best_acq + "_expval=" + str(cur_best_exp))
+plt.show
+
+print("Coordinates with largest wind speed: latitude=" + str(x_best[1]) + ", longitude=" + str(x_best[0])
+      + "\nAcquistion function = " + cur_best_acq + ", exploration weight = " + str(cur_best_exp))
+print("=" * 90)
 
 """
 # Plot of guesses
